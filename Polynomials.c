c9f14c248697dd418c25353b581d1b383d597beb
= <=>
2021-03-08 17:07:36 +0800

    add c

---
+++ Polynomials.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct PolyNode{
+    int coef;
+    int exp;
+    struct PolyNode *link;
+}
+typedef struct PolyNode* Polynomials;
+
+void Attach(int c, int e, Polynomials)
+
+PolyList ReadList()
+{
+
+}
