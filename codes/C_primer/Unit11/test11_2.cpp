b8cf09379e06fcdab306662f2f64ca2f9f55e581
= <=>
2021-09-17 22:34:25 +0800

    2021-09-17

--- codes/C_primer/Unit11/test11_2.cpp
+++ codes/C_primer/Unit11/test11_2.cpp
@@ -7,21 +7,37 @@
 #include <string>
 using namespace std;
 
+string &trans(string &s)
+{
+    for (int p = 0; p < s.size(); p++)  
+    {
+        cout<<s[p];
+        if (s[p] >= 'A' && s[p] <= 'Z')
+        {
+            s[p] -= ('A' - 'a');
+        }
+        else if (s[p] == ',' || s[p] == '.')
+        {
+            s.erase(p, 1);      //此处erase函数返回删除后的string，所有size减少1，是的for循环不能使用p!=s.seiz()
+        }
+    }
+    cout << " " << s << endl;
+    return s;
+}
 int main(int argc, char *argv[])
 {
 
-    cout<<argv[1];
-    ifstream in(argv[1]);
+    ifstream in(".\\word_count");
     if (!in)
     {
         cout << "打开输入文件失败！" << endl;
         exit(1);
     }
 
-    map<string, size_t> word_count;   //size_t -->long unsigened int
+    map<string, size_t> word_count; //size_t -->long unsigened int
     string s;
     while (in >> s)
-        ++word_count[s];
+        ++word_count[trans(s)];
     for (const auto &d : word_count)
         cout << d.first << "出现了" << d.second << "次" << endl;
     return 0;
---
+++ codes/C_primer/Unit11/test11_7.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+vpid add_family(map<string,vector<string>> &familys,const string &family)
+{
+    
+}
+
+int main(int argc, char *argv[])
+{
+    map<string,vector<string>> familys;
+
+}
